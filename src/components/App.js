fb533537216353a528b1ac00ea2eaf1c6e0fcae6
Santeri Kilpi <s.kilpi@live.fi>
2020-04-12 16:58:53 +0300

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,168 @@
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import React, { Component } from "react";
+import api from "../lib/api";
+import CustomTable from "./CustomTable";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userTableData: [],
+      projectTableData: [],
+      userTableStatus: "INITIAL",
+      projectTableStatus: "INITIAL",
+    };
+  }
+
+  renderError(status) {
+    if (status === "ERROR") {
+      return (
+        <Box marginTop="20px">
+          <Typography color="error">
+            We had problems fetching your data. Please try again.
+          </Typography>
+        </Box>
+      );
+    }
+  }
+
+  renderLoading(status) {
+    if (status === "LOADING") {
+      return (
+        <Box marginTop="20px">
+          <CircularProgress color="primary" />
+        </Box>
+      );
+    }
+  }
+
+  fetchUserData = async () => {
+    this.setState({ userTableStatus: "LOADING" });
+    await api
+      .getUsersDiff()
+      .then((response) => {
+        if (response.code === 200) {
+          this.setState((state) => {
+            const userTableData = state.userTableData.concat(response.data);
+            return {
+              userTableData,
+              userTableStatus: "COMPLETE",
+            };
+          });
+        } else {
+          this.setState({ userTableStatus: "ERROR" });
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        this.setState({ userTableStatus: "ERROR" });
+      });
+  };
+
+  fetchProjectData = async () => {
+    this.setState({ projectTableStatus: "LOADING" });
+    await api
+      .getProjectsDiff()
+      .then((response) => {
+        if (response.code === 200) {
+          this.setState((state) => {
+            const projectTableData = state.projectTableData.concat(
+              response.data
+            );
+            return {
+              projectTableData,
+              projectTableStatus: "COMPLETE",
+            };
+          });
+        } else {
+          this.setState({ projectTableStatus: "ERROR" });
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        this.setState({ projectTableStatus: "ERROR" });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchUserData();
+    this.fetchProjectData();
+  }
+
+  render() {
+    let theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: "#2197F3",
+        },
+      },
+      overrides: {
+        MuiPaper: {
+          root: {
+            padding: "10px",
+            marginBottom: "10px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          },
+        },
+        MuiTableCell: {
+          head: {
+            fontWeight: 600,
+          },
+        },
+        MuiButton: {
+          root: {
+            textTransform: "none",
+            margin: "20px",
+          },
+        },
+      },
+    });
+    responsiveFontSizes(theme);
+    return (
+      <ThemeProvider theme={theme}>
+        <Container component={Paper}>
+          <CustomTable tableData={this.state.userTableData} />
+          {this.renderLoading(this.state.userTableStatus)}
+          {this.renderError(this.state.userTableStatus)}
+          <Box>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.fetchUserData}
+            >
+              Load More
+            </Button>
+          </Box>
+        </Container>
+        <Container component={Paper}>
+          <CustomTable tableData={this.state.projectTableData} />
+          {this.renderLoading(this.state.projectTableStatus)}
+          {this.renderError(this.state.projectTableStatus)}
+          <Box>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.fetchProjectData}
+            >
+              Load More
+            </Button>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CustomTable.js
@@ -0,0 +1,83 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import React, { Component } from "react";
+
+export default class CustomTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chronological: false,
+    };
+  }
+
+  renderTableBody() {
+    if (this.props.tableData.length === 0) return;
+    let tableData = this.sortTableData();
+    return tableData.map((data) => {
+      return (
+        <TableRow key={data.id}>
+          <TableCell>{new Date(data.timestamp).toDateString()}</TableCell>
+          <TableCell>{data.id}</TableCell>
+          <TableCell>{data.diff[0].oldValue}</TableCell>
+          <TableCell>{data.diff[0].newValue}</TableCell>
+        </TableRow>
+      );
+    });
+  }
+
+  sortDate = () => {
+    if (this.state.chronological) this.setState({ chronological: false });
+    else this.setState({ chronological: true });
+  };
+
+  sortTableData() {
+    const sortedData = this.props.tableData.sort((a, b) => {
+      return this.compareDates(a.timestamp, b.timestamp);
+    });
+    if (!this.state.chronological) {
+      return sortedData.reverse();
+    } else return sortedData;
+  }
+
+  compareDates(a, b) {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    return 0;
+  }
+
+  render() {
+    let theme = createMuiTheme({
+      overrides: {
+        MuiTableCell: {
+          head: {
+            fontWeight: 600,
+          },
+        },
+      },
+    });
+    responsiveFontSizes(theme);
+    return (
+      <Table id="users">
+        <TableHead>
+          <TableRow>
+            <TableCell onClick={this.sortDate} align="left">
+              Date
+            </TableCell>
+            <TableCell align="left">User ID</TableCell>
+            <TableCell align="left">Old Value</TableCell>
+            <TableCell align="left">New Value</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>{this.renderTableBody()}</TableBody>
+      </Table>
+    );
+  }
+}
